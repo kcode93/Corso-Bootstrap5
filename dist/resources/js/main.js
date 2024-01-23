8d74c2220a08b56e5e843c8083996130c5b06249
Kenneth Garcia <kennethgarcia12@hotmail.com>
2024-01-23 14:23:22 -0800

    if applied, this commit adds a shadows to navbar on scroll

--- dist/resources/js/main.js
+++ dist/resources/js/main.js
@@ -5,8 +5,10 @@ function navStyleOnScroll(){
     window.addEventListener('scroll', () => {
         if(window.scrollY > 50){
             navbar.classList.add('bg-dark');
+            navbar.classList.add('shadow');
         }else{
             navbar.classList.remove('bg-dark');
+            navbar.classList.remove('shadow');
         }
     });
 }
